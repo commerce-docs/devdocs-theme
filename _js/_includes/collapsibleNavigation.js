52b93293d73a6e0d4105b998707384354e9d9414
Eugene Bannykh <ybannykh@magento.com>
2018-04-25 09:15:24 -0500

    Collapsible - do not show “expand all” on non-tree

--- _js/_includes/collapsibleNavigation.js
+++ _js/_includes/collapsibleNavigation.js
@@ -24,7 +24,7 @@
 		function Plugin ( element, options ) {
 			this.element = element;
 
-			this.settings = $.extend( {}, defaults, options );
+			this.options = $.extend( {}, defaults, options );
 			this._defaults = defaults;
 			this._name = pluginName;
 			this.init();
@@ -32,16 +32,19 @@
 
 		// Avoid Plugin.prototype conflicts
 		$.extend( Plugin.prototype, {
+
 			init: function() {
-        this.items = $( this.element ).find( this.settings.itemSelector );
+        this.items = $( this.element ).find( this.options.itemSelector );
         this.initMenuItems( this.items );
-				this.addExpandAllToggle();
+				if ( this.items.hasClass(this.options.hasChildrenClass) ) {
+					this.addExpandAllToggle();
+				}
 			},
 
       initMenuItems: function( items ) {
 
         var plugin = this;
-        var settings = plugin.settings;
+        var options = plugin.options;
         var n = 0;
 
         items.each ( function () {
@@ -49,11 +52,11 @@
           var item = $(this);
           item.attr('role', 'treeitem');
 
-          var submenu = item.find( settings.submenuSelector );
+          var submenu = item.find( options.submenuSelector );
 
           // If this item is active, traverse the tree up and open each parent
-          if ( item.hasClass ( settings.itemActiveClass ) ) {
-            var parents = item.parents( settings.itemSelector );
+          if ( item.hasClass ( options.itemActiveClass ) ) {
+            var parents = item.parents( options.itemSelector );
             plugin.openSubmenu( item, undefined, 0 );
             parents.each( function () {
               plugin.openSubmenu( $(this), undefined, 0 );
@@ -72,7 +75,7 @@
           if ( item.find('> a').length === 0 ) {
 
             item
-              .addClass(settings.itemToggleClass)
+              .addClass(options.itemToggleClass)
               .find('> span')
               .on('click',
                 { item: item,
@@ -89,7 +92,7 @@
             n++;
             var submenuId = 'collapsible-menu-' + n;
             var toggle = $('<button />')
-              .addClass( settings.toggleClass)
+              .addClass( options.toggleClass)
               .attr('aria-controls', submenuId).
 							insertBefore( $( submenu ) );
 
@@ -97,7 +100,7 @@
             $( submenu ).attr('id', submenuId)
               .attr('role', 'group');
 
-            $( item ).addClass( settings.hasChildrenClass );
+            $( item ).addClass( options.hasChildrenClass );
 
               //Assign events
             toggle.on('click',
@@ -133,16 +136,16 @@
 
       openSubmenu: function ( item, submenu, speed ) {
 
-        item.removeClass( this.settings.closedClass ).
-          addClass(this.settings.openClass).
+        item.removeClass( this.options.closedClass ).
+          addClass(this.options.openClass).
           attr('aria-expanded', 'true');
 
         if( typeof submenu === "undefined" ) {
-          submenu = item.find( this.settings.submenuSelector );
+          submenu = item.find( this.options.submenuSelector );
         }
 
         if (typeof speed === "undefined" ) {
-          speed = this.settings.speed;
+          speed = this.options.speed;
         }
 
         submenu.slideDown( speed );
@@ -151,16 +154,16 @@
 
       closeSubmenu: function ( item, submenu, speed ) {
 
-        item.removeClass( this.settings.openClass ).
-          addClass(this.settings.closedClass).
+        item.removeClass( this.options.openClass ).
+          addClass(this.options.closedClass).
           attr('aria-expanded', 'false');
 
         if( typeof submenu === "undefined" ) {
-          submenu = item.find( this.settings.submenuSelector );
+          submenu = item.find( this.options.submenuSelector );
         }
 
         if (typeof speed === "undefined" ) {
-          speed = this.settings.speed;
+          speed = this.options.speed;
         }
 
         submenu.slideUp( speed );
