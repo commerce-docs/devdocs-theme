fc36c6d0b19e9bfc1feb6fdc3427d70ecf9d4953
Eugene Bannykh <ybannykh@magento.com>
2018-05-07 12:31:34 -0500

    Get rid of the unused JS, update some vendor scripts

--- _js/_includes/videos.js
+++ _js/_includes/videos.js
@@ -1,27 +1,35 @@
-// Video
+/*!
+ * Videos
+ * Depends on the Bootstrap Affix plugin
+ * author: ybannykh@magento.com
+*/
 
-var $video = $('.video-player');
+$(function() {
 
-if ( $video.length ) {
-  var $videoWrap = $('.video-player-wrap'),
-      videoOffsetTop = $video.outerHeight(true) + $video.offset().top;
+  var $video = $('.video-player');
 
-  $video.affix({
-    offset: {
-      top: videoOffsetTop,
-      bottom: function () {
-        return (this.bottom = $('#footer').outerHeight(true))
+  if ( $video.length ) {
+    var $videoWrap = $('.video-player-wrap'),
+        videoOffsetTop = $video.outerHeight(true) + $video.offset().top;
+
+    $video.affix({
+      offset: {
+        top: videoOffsetTop,
+        bottom: function () {
+          return (this.bottom = $('#footer').outerHeight(true))
+        }
       }
-    }
-  });
+    });
 
-  $video.on('affix.bs.affix', function () {
-    $videoWrap.addClass('embed-responsive-16by9');
-  });
+    $video.on('affix.bs.affix', function () {
+      $videoWrap.addClass('embed-responsive-16by9');
+    });
 
-  $video.on('affix-top.bs.affix', function () {
-    $videoWrap.removeClass('embed-responsive-16by9');
-  });
+    $video.on('affix-top.bs.affix', function () {
+      $videoWrap.removeClass('embed-responsive-16by9');
+    });
 
 
-}
+  }
+
+});
